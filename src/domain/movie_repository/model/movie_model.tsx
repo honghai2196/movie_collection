dea1f9eef457404c91186e4d87fb7338c6392d20
Hải Đặng <honghai2196@outlook.com>
2021-11-04 02:31:46 +0700

    format model

--- src/domain/movie_repository/model/movie_model.tsx
+++ src/domain/movie_repository/model/movie_model.tsx
@@ -1,32 +1,32 @@
-export interface MovieProps{
-    id:                 string
-    rank:               string
-    title:              string
-    year:               string
-    image:              string
-    crew:               string
-    imDbRating:         string
-    imDbRatingCount:    string
+export interface MovieProps {
+  id: string
+  rank: string
+  title: string
+  year: string
+  image: string
+  crew: string
+  imDbRating: string
+  imDbRatingCount: string
 }
 
 export class Movie {
-    public id:          string
-    public rank:        string
-    public title:       string
-    public year:        string
-    public image:       string
-    public crew:        string
-    public imDbRating:  string
-    public imDbRatingCount: string
+  public id: string
+  public rank: string
+  public title: string
+  public year: string
+  public image: string
+  public crew: string
+  public imDbRating: string
+  public imDbRatingCount: string
 
-    constructor(props: MovieProps) {
-        this.id                 = props.id
-        this.rank               = props.rank
-        this.title              = props.title
-        this.year               = props.year
-        this.image              = props.image
-        this.crew               = props.crew
-        this.imDbRating         = props.imDbRating
-        this.imDbRatingCount    = props.imDbRatingCount
-    }
+  constructor(props: MovieProps) {
+    this.id = props.id
+    this.rank = props.rank
+    this.title = props.title
+    this.year = props.year
+    this.image = props.image
+    this.crew = props.crew
+    this.imDbRating = props.imDbRating
+    this.imDbRatingCount = props.imDbRatingCount
+  }
 }
