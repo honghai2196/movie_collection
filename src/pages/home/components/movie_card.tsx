4f547fb944b6761082365f0f5cc4c4a1e0a89229
Hải Đặng <honghai2196@outlook.com>
2021-11-04 14:09:37 +0700

    add styles home

--- src/pages/home/components/movie_card.tsx
+++ src/pages/home/components/movie_card.tsx
@@ -1,4 +1,11 @@
-import { Box, Grid, Typography } from "@material-ui/core"
+import {
+  Box,
+  Grid,
+  Hidden,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core"
 import { Movie } from "../../../domain/movie_repository/model/movie_model"
 import Image from "material-ui-image"
 import MovieHover from "./movie_hover"
@@ -20,8 +27,62 @@ const setVoteClass = (vote: string) => {
   return "red"
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  movie: {
+    backgroundColor: "#373b69",
+    borderRadius: "10px",
+    boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.1)",
+    overflow: "hidden",
+    position: "relative",
+    margin: "1rem",
+    width: "300px",
+    "&:hover": {
+      "& $movieOver": {
+        transform: "translateY(0%)",
+      },
+    },
+  },
+  img: {
+    maxWidth: "100%",
+  },
+  movieInfo: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "1rem",
+  },
+  movieInfo_movieTitle: {
+    color: "rgb(255, 255, 255)",
+    margin: 0,
+  },
+  movieInfo_movieRating: {
+    backgroundColor: "#22254b",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    fontWeight: "bold",
+  },
+  movieOver: {
+    backgroundColor: "white",
+    color: "black",
+    padding: "1rem",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    overflow: "auto",
+    maxHeight: "100%",
+    transform: "translateY(100%)",
+    transition: "transform 0.3s ease-in-out",
+
+    "& h5": {
+      fontWeight: "bolder",
+    },
+  },
+}))
+
 const MovieCard = (movie: Movie) => {
   const history = useHistory()
+  const classes = useStyles()
 
   function onMovieClicked() {
     history.push("/detail", movie)
@@ -29,21 +90,29 @@ const MovieCard = (movie: Movie) => {
 
   return (
     <Grid item>
-      <Box className={"movie " + movie.id} onClick={onMovieClicked}>
-        <Image src={movie.image} alt={movie.title + " (" + movie.year + ")"} />
-        <Box className="movie-info">
-          <Typography variant="body1" className="movie-title">
+      <Box className={classes.movie} onClick={onMovieClicked}>
+        <Image
+          className={classes.img}
+          src={movie.image}
+          alt={movie.title + " (" + movie.year + ")"}
+        />
+        <Box className={classes.movieInfo}>
+          <Typography variant="body1" className={classes.movieInfo_movieTitle}>
             {movie.title}
           </Typography>
           <Box
             display="inline"
-            className={`movie-rating ${setVoteClass(movie.imDbRating)}`}
+            className={
+              classes.movieInfo_movieRating +
+              " " +
+              setVoteClass(movie.imDbRating)
+            }
           >
             {movie.imDbRating}
           </Box>
         </Box>
 
-        <MovieHover {...movie} />
+        <MovieHover movie={movie as Movie} className={classes.movieOver} />
       </Box>
     </Grid>
   )
--- src/pages/home/components/movie_hover.tsx
+++ src/pages/home/components/movie_hover.tsx
@@ -1,16 +1,22 @@
 import { Box, Typography } from "@material-ui/core"
+import React from "react"
 import { Movie } from "../../../domain/movie_repository/model/movie_model"
 
-const MovieHover = (movie: Movie) => {
+type Props = {
+  movie?: Movie
+  className?: string
+}
+
+const MovieHover: React.FC<Props> = ({ movie, className }) => {
   return (
-    <Box className="movie-over">
+    <Box className={className}>
       <Typography variant="h5">Overview:</Typography>
-      <Typography>{movie.title}</Typography>
-      <Typography>{"Publish year: " + movie.year}</Typography>
-      <Typography>{"Casts: " + movie.crew}</Typography>
-      <Typography>{"Ranking: " + movie.rank}</Typography>
-      <Typography>{"IMDB Rating: " + movie.imDbRating}</Typography>
-      <Typography>{"IMDB Rating Count: " + movie.imDbRatingCount}</Typography>
+      <Typography>{movie?.title}</Typography>
+      <Typography>{"Publish year: " + movie?.year}</Typography>
+      <Typography>{"Casts: " + movie?.crew}</Typography>
+      <Typography>{"Ranking: " + movie?.rank}</Typography>
+      <Typography>{"IMDB Rating: " + movie?.imDbRating}</Typography>
+      <Typography>{"IMDB Rating Count: " + movie?.imDbRatingCount}</Typography>
     </Box>
   )
 }
